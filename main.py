a99cde26ae15253dc5e0274282ce570b57f31443
Lorenzo Fioroni <lor.fioroni@gmail.com>
2021-11-27 18:10:04 +0100

    parameter change

--- main.py
+++ main.py
@@ -24,14 +24,14 @@ sim = Simulation(env, 0.1)
 
 for i in range(50):
     if np.random.rand()<0.5:
-        target0 = np.array([0, 4.5])
-        target1 = np.array([0, 5.5])
-        start = np.array([10, np.random.uniform(4.5, 5.5)])
+        target0 = np.array([0, 4.1])
+        target1 = np.array([0, 5.9])
+        start = np.array([10, np.random.uniform(4.1, 5.9)])
     else:
-        target0 = np.array([10, 4.5])
-        target1 = np.array([10, 5.5])
-        start = np.array([0, np.random.uniform(4.5, 5.5)])
-    sim.add_agent(Agent(env, np.abs(np.random.normal(loc=3, scale=1.5)), start, target0, target1))
+        target0 = np.array([10, 4.1])
+        target1 = np.array([10, 5.9])
+        start = np.array([0, np.random.uniform(4.1, 5.9)])
+    sim.add_agent(Agent(env, np.abs(np.random.uniform(0, 10)), start, target0, target1))
 
 
 # Running the simulation 
